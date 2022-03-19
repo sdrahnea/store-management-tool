e4282790abebad2fe820cc958c329913c9fd59d4
Sergiu Drahnea <s.drahnea@gmail.com>
2022-03-19 23:14:30 +0200

    init commit

---
+++ src/main/java/com/smt/StoreManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.smt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagementToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smt/StoreManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
